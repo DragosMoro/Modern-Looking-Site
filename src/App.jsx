8b7ceb169a443a57c0d6bee273819da1f6b5f361
DragosMoro <morozan.dragos@gmail.com>
2023-07-26 19:45:34 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import styles from './style'
-const App = () =>  (
-    <div className='bg-primary w-full overflow-hidden'>
-      <div className={'${styles.paddingX}'}>
-        
-
+import React from "react";
+import styles from "./style";
+const App = () => (
+  <div className='bg-primary w-full overflow-hidden'>
+    <div className={'${styles.paddingX} ${styles.flexCenter}'}>
+      <div className={'${styles.boxWidth}'}>navbar</div>
+    </div>
+    <div className={'bg-primary ${styles.flexStart}'}>
+      <div className={'${styles.boxWidth}'}>hero </div>
+    </div>
+    <div className={'bg-primary ${styles.paddingX} ${styles.flexStart}'}>
+      <div className={'${styles.boxWidth}'}>
+        Stats
+        Business
+        Billing
+        CardDeal
+        Testimonials
+        Clients
+        CTA
+        Footer 
       </div>
     </div>
-  )
+    
+  </div>
+);
 
-
-export default App
+export default App;
