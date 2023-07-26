f8f765f3d48d6300d8da828198f315cffc190225
DragosMoro <morozan.dragos@gmail.com>
2023-07-26 23:01:36 +0300

    update nav and hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
-import React from "react";
 import styles from "./style";
+import {Navbar, Hero, Stats,Business,Billing,CardDeal,Testimonials,Clients,CTA,Footer } from "./components";
+
+
 const App = () => (
   <div className='bg-primary w-full overflow-hidden'>
-    <div className={'${styles.paddingX} ${styles.flexCenter}'}>
-      <div className={'${styles.boxWidth}'}>navbar</div>
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar/>
+        </div>
     </div>
-    <div className={'bg-primary ${styles.flexStart}'}>
-      <div className={'${styles.boxWidth}'}>hero </div>
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero/> 
+      </div>
     </div>
-    <div className={'bg-primary ${styles.paddingX} ${styles.flexStart}'}>
-      <div className={'${styles.boxWidth}'}>
-        Stats
-        Business
-        Billing
-        CardDeal
-        Testimonials
-        Clients
-        CTA
-        Footer 
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats/>
+        <Business/>
+        <Billing/>
+        <CardDeal/>
+        <Testimonials/>
+        <Clients/>
+        <CTA/>
+        <Footer/> 
       </div>
     </div>
     
