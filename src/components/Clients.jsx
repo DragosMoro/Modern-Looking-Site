94a881fc9bc2b73de44759d9f11faeb4d99c496e
DragosMoro <morozan.dragos@gmail.com>
2023-07-27 22:16:47 +0300

    final-touches

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
-
+import {clients} from "../constants"
+import styles from "../style"
 const Clients = () => {
   return (
-    <div>Clients</div>
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map((client) => (
+          <div key={clients.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] `}>
+            <img src={client.logo} alt="client" className="sm:w-[192px] w-[100px] object-contain cursor-pointer hover:brightness-200 transition duration-500"/>
+          </div>
+
+        ))}
+
+      </div>
+
+    </section>
   )
 }
 
