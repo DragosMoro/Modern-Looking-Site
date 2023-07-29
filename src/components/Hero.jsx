bc18cb225a5f9bebef416b53fe8b31710fa3f2a6
DragosMoro <morozan.dragos@gmail.com>
2023-07-29 23:10:28 +0300

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
             <span className="text-gradient">Generation</span>
           </h1>
           <div className="ss:flex hidden md:mr-4 mr-0">
-            <GetStarted />
+            <GetStarted  />
           </div>
         </div>
         <h1 className=" font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading[100px] leading-[75px] w-full">Payment Method.</h1>
