d379ab6212713ec50af8018c6b2dcf9fc9a86983
DragosMoro <morozan.dragos@gmail.com>
2023-07-29 23:06:43 +0300

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 const Button = ({styles}) => {
   return (
-    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] rounded-[12px] text-primary outline-none ${styles} hover:`}>
+    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] rounded-[12px] text-primary outline-none ${styles} hover:brightness-75 transition duration-500`}>
     Get Started
     </button>
   )
